07df6570ddfc45054fa8f3176226d369167b8cb3
cou723 <66350686+cou723@users.noreply.github.com>
2022-12-06 20:12:44 +0900

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,17 +1,37 @@
+# ls(exa)
+alias ls='exa --grid --icons --git'
+alias ll='exa --long --icons --git'
+alias lt='exa --tree --icons --git'
+alias ltl='exa --tree --long --icons --git --level=2'
+
+# util
 alias cd~="cd /mnt/c/Users/Cou/"
+alias norm="norminette"
+alias witch="which"
+alias jan='LANG=ja_JP.utf8 man'
+
+# git
 alias ga="git add"
 alias ga.="git add ."
 alias gicm="git commit -m"
 alias gpsh="git push"
 alias gpl="git pull"
 alias gc="git checkout"
+alias gc.="git checkout ."
 alias gb="git branch"
 alias gm="git merge"
-alias norm="norminette"
-alias witch="which"
+alias gd="git diff"
+
+# npm
+alias n="npm"
+alias nr="npm run"
+alias pi="pnpm install"
 
 export DENO_INSTALL="/home/cou/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
 export PATH="/home/cou/.local/bin:$PATH"
 export MANPATH="/mnt/c/Users/Cou/dev/_42/so_long/mlx_linux/man":(manpath)
-alias jan='LANG=ja_JP.utf8 man'
+export PNPM_HOME="/home/cou/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"
+
+starship init fish | source

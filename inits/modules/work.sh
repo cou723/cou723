bd31ad2c61a16b36bac751bef1b9c7e83a19048a
cou723 <66350686+cou723@users.noreply.github.com>
2023-09-06 17:18:39 +0900

    Create work.sh

---
+++ inits/modules/work.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Hackgen NFを表示用フォントにすることを忘れずに
+
+# rust
+sudo apt install -y gcc
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# utils
+sudo dnf install -y curl zip unzip wget git
+$HOME/.cargo/bin/cargo install exa bat
+
+# zsh
+sudo dnf install zsh -y
+curl -fsSL https://git.io/zinit-install | sh
+sudo sed --in-place -e '/auth.*required.*pam_shells.so/s/required/sufficient/g' /etc/pam.d/chsh
+chsh -s $(which zsh)
+
+# homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+/home/linuxbrew/.linuxbrew/bin/brew install ghq chezmoi brew
+
+chezmoi init cou723
+chezmoi apply
+
+# chezmoi
+mkdir -p ~/.config/chezmoi
+echo "[edit]" >> ~/.config/chezmoi/chezmoi.toml
+echo '    command = "code"' >> ~/.config/chezmoi/chezmoi.toml
+echo '    args = ["--wait"]' >> ~/.config/chezmoi/chezmoi.toml

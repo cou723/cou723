2abf631b42087f1e99188aece746d8ef6e67df5f
cou <courange.c@gmail.com>
2024-02-10 20:43:28 +0900

    update

--- inits/modules/python.sh
+++ inits/modules/python.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+sudo apt install -y build-essential libffi-dev libssl-dev zlib1g-dev liblzma-dev libbz2-dev libreadline-dev libsqlite3-dev libopencv-dev tk-dev git
 git clone git://github.com/yyuu/pyenv.git ~/.pyenv
 pyenv install 3.11.4

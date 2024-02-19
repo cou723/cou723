49bbcb4aad12f220e268c3b6be64af1896856a8b
cou723 <66350686+cou723@users.noreply.github.com>
2024-02-19 15:15:26 +0900

    Update python.sh

--- inits/modules/python.sh
+++ inits/modules/python.sh
@@ -2,3 +2,4 @@
 sudo apt install -y build-essential libffi-dev libssl-dev zlib1g-dev liblzma-dev libbz2-dev libreadline-dev libsqlite3-dev libopencv-dev tk-dev git
 git clone git://github.com/yyuu/pyenv.git ~/.pyenv
 pyenv install 3.11.4
+pip3 install numpy matplotlib pandas 

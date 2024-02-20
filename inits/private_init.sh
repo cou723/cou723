a3f2870320d34db5adca89828ad7de761b3beef0
cou723 <66350686+cou723@users.noreply.github.com>
2024-02-20 11:41:40 +0900

    Update private_init.sh

--- inits/private_init.sh
+++ inits/private_init.sh
@@ -4,6 +4,7 @@ curl https://raw.githubusercontent.com/cou723/cou723/main/inits/modules/copy-ssh
 curl https://raw.githubusercontent.com/cou723/cou723/main/inits/modules/python.sh | sh
 curl https://raw.githubusercontent.com/cou723/cou723/main/inits/modules/web.sh | sh
 
+git config --global push.default current
 git config --global --add --bool push.autoSetupRemote true
 git config --global user.email "courange.c@gmail.com"
 git config --global user.name "cou723"

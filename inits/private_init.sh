a3814fab1764582851eb993bfc491b25fb23521d
cou723 <66350686+cou723@users.noreply.github.com>
2024-06-14 14:13:50 +0900

    Update private_init.sh

--- inits/private_init.sh
+++ inits/private_init.sh
@@ -8,3 +8,13 @@ git config --global push.default current
 git config --global --add --bool push.autoSetupRemote true
 git config --global user.email "courange.c@gmail.com"
 git config --global user.name "cou723"
+
+# WSLg用日本語化
+cat << 'EOS' | sudo tee /etc/fonts/local.conf
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+    <dir>/mnt/c/Windows/Fonts</dir>
+</fontconfig>
+<!-- Created by bash script from https://astherier.com/blog/2021/07/windows11-wsl2-wslg-japanese/ -->
+EOS
